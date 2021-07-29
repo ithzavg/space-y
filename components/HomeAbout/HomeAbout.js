1257d89dff12528fa572561f1b924cbb1b79fef0
ithzavg <ithgarvi@gmail.com>
2021-07-28 21:35:11 -0500

    tailwind configuration, routing, about layout

---
+++ components/HomeAbout/HomeAbout.js
@@ -0,0 +1,25 @@
+/*Import next Components */
+import Image from 'next/image';
+
+export default function HomeAbout({ about }){
+    console.log(about)
+    return(
+        <section className=" min-h-screen fixed z-10 w-full flex flex-col justify-center  text-white">
+           <div className="bg-gray-400 bg-opacity-75 w-full h-1/2 px-10 text-lg shadow">
+                <p className="font-russo text-3xl  text-center">About</p>
+                <p>{about.ceo}</p>
+                <p className="mb-3">{about.summary}</p>
+
+                <div>
+                    <Image src="/assets/img/elonMusk.png"  width={100} height={50} alt="CEO Elon Musk"></Image>
+                    <p>{about.ceo}</p>
+
+                    <Image src="/assets/img/GwynneShotwell.png" width={100} height={50} alt="COO Gwynne Shotwell"></Image>
+                    <p>{about.coo}</p>
+                </div>
+           </div>
+            
+        </section>
+    )
+}
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+/*Import next Components */
+import Head from 'next/head';
+
+/*Import styles */
+import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return(
+    <div>
+      <Head>
+        <title>Space-Y</title>
+        <meta name="description" content="Space Y, a YEMA challenge" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet"></link> 
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
+
 }
 
 export default MyApp
