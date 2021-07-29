b23151882edd4e902245d8ed06032e91664bee03
ithzavg <ithgarvi@gmail.com>
2021-07-29 00:55:39 -0500

    Card component, homepage about modifications

--- components/HomeAbout/HomeAbout.js
+++ components/HomeAbout/HomeAbout.js
@@ -2,20 +2,42 @@
 import Image from 'next/image';
 
 export default function HomeAbout({ about }){
-    console.log(about)
+    
+
     return(
         <section className=" min-h-screen fixed z-10 w-full flex flex-col justify-center  text-white">
            <div className="bg-gray-400 bg-opacity-75 w-full h-1/2 px-10 text-lg shadow">
                 <p className="font-russo text-3xl  text-center">About</p>
-                <p>{about.ceo}</p>
                 <p className="mb-3">{about.summary}</p>
 
-                <div>
-                    <Image src="/assets/img/elonMusk.png"  width={50} height={50} alt="CEO Elon Musk"/>
-                    <p>{about.ceo}</p>
+                <p className="font-bold">CEO: {about.ceo}</p>
+                <p className="font-bold">COO: {about.coo}</p>
 
-                    <Image src="/assets/img/GwynneShotwell.png" width={50} height={50} alt="COO Gwynne Shotwell"></Image>
-                    <p>{about.coo}</p>
+                <div className="flex justify-center">
+                    <Image className="mr-2" src="/elonMusk.png"  width={105} height={105} alt="CEO Elon Musk"/>
+                    <Image className="object-center"  objectFit="fill" src="/GwynneShotwell.png" width={105} height={105} alt="COO Gwynne Shotwell"></Image>
+                    
+                </div>
+
+                <div>
+                    <p className="text-center font-bold">Find Space X on social media</p>
+
+                    
+
+                    <div className="flex mb-2 justify-center">
+                        <span className=" px-2 mr-2 leading-none bg-yellow-700 text-yellow-200 rounded-full uppercase tracking-wide text-xs">
+                            <a href={about.links.elon_twitter} target="_blank" rel="noreferrer">Elon Twitter</a>
+                        </span>
+                        <span className=" px-2 mr-2 leading-none bg-yellow-700 text-yellow-200 rounded-full uppercase tracking-wide text-xs">
+                            <a href={about.links.flickr} target="_blank" rel="noreferrer">Flickr</a>
+                        </span>
+                        <span className=" px-2 mr-2 leading-none bg-yellow-700 text-yellow-200 rounded-full uppercase tracking-wide text-xs">
+                            <a href={about.links.twitter} target="_blank" rel="noreferrer">Twitter</a>
+                        </span>
+                        <span className=" px-2 mr-2 leading-none bg-yellow-700 text-yellow-200 rounded-full uppercase tracking-wide text-xs">
+                            <a href={about.links.website} target="_blank" rel="noreferrer">Website</a>
+                        </span>
+                    </div>
                 </div>
            </div>
             
--- pages/launches.js
+++ pages/launches.js
@@ -1,15 +1,29 @@
 /*Import libraries */
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+/*Import next Components */
 import Navbar from '../components/Navbar/Navbar';
+import Card from '../components/Card/Card';
 
 const Launches = ({ launches }) =>{
-    console.log(launches)
+ 
     return (
         <div >
             <Navbar></Navbar>
 
-            <section className="min-h-screen bg-gray-800">
-
+            <section className="min-h-screen bg-gray-800 grid lg:grid-cols-4 md:grid-cols-2 p-10 gap-4">
+              {launches.map(launch => {
+                let img = launch.links.flickr_images.length === 0 ? "/default.jpg" : launch.links.flickr_images[0]
+                return(
+                  <Card 
+                    key={launch.id}
+                    mission_name ={launch.mission_name} 
+                    date={launch.launch_date_local}
+                    wikipedia={launch.links.wikipedia} 
+                    image = {img}></Card>
+                )
+              })}
+                
             </section>
         </div>
     )
@@ -28,6 +42,7 @@ export async function getStaticProps(){
       query: gql `
       query GET_LAUNCH_INFO {
         launchesPast(limit: 10) {
+          id
           mission_name
           launch_date_local
           links {
